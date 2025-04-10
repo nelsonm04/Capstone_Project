9a382be971f57f75211255118d086726845bfc78
Nelson Mojica <nelsonm43@icloud.com>
2025-04-10 11:42:43 -0400

    Can switch screens from any screen, built basic GUI

--- src/main/java/com/example/capstone/SocialController.java
+++ src/main/java/com/example/capstone/SocialController.java
@@ -1,4 +1,42 @@
 package com.example.capstone;
 
-public class SocialController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SocialController implements Initializable {
+
+    @FXML
+    private GridPane calendarGrid;
+    @FXML
+    private Button addEventButton, mainButton, settingButton, socialButton, eventButton;
+    @FXML
+    private Label monthYear;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+        // Switches the screen when clicked on
+        mainButton.setOnAction(e -> handleScreenSwitch("MainScreen"));
+        settingButton.setOnAction(e -> handleScreenSwitch("settingScreen"));
+        socialButton.setOnAction(e -> handleScreenSwitch("socialScreen"));
+        eventButton.setOnAction(e -> handleScreenSwitch("eventScreen"));
+
+    }
+
+    private void handleScreenSwitch(String screenName) {
+        //Switching screens through buttons
+        try {
+            CapstoneApplication.setRoot(screenName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
